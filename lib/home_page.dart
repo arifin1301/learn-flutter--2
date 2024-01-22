998d72ac63c794d2f744854dbd1ac722673ba5d6
dion <juniordion7@gmail.com>
2024-01-23 01:56:05 +0800

    add title place

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,8 +6,40 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Image.asset("assets/image.png"),
+            SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 30,
+              ),
+              child: Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Pantai Teluk Penyu",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text(
+                        "Cilacap, Jawa Tengah",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w300, fontSize: 12),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
